5af3f01d30a6a33a13134c7aa3a8d82416a7871e
eduardotecnologo <edudeveloper@Eduardo-MacBook-Pro.local>
2021-09-02 23:27:17 -0300

    create project api and front

---
+++ front/src/layouts/DefaultTemplate.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+     <h1>tsrstrsrsr</h1>
+  </div>
+</template>
+<script>
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default {
+
+};
+</script>
+<style lang="css" scoped>
+@import url('../assets/css/ifast.css');
+@import url('../assets/css/reset.css');
+</style>
---
+++ front/src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import DefaultTemplate from './layouts/DefaultTemplate.vue'
+
+createApp(DefaultTemplate).mount('#app')
